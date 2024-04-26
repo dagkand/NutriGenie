6d813ea48ba613467f22ea97d02ee3cfefa9d21e
dagkand <dagkandersen@gmail.com>
2024-04-26 15:11:20 +0200

    temp

---
+++ src/components/Graph.tsx
@@ -0,0 +1,23 @@
+import '../App.css'
+
+function Graph() {
+
+  return (
+    <div className='motivation'>
+        <h2>Motivation for Learning BMR and Macro Split for Fitness</h2>
+        <p>Understanding concepts such as Basal Metabolic Rate (BMR) and macronutrient splits is essential for individuals pursuing fitness and bodybuilding goals. Here's why:</p>
+        <h3>Optimizing Nutrition</h3>
+        <p>Knowledge of your BMR enables you to tailor your calorie intake to match your body's energy needs. By calculating your BMR and considering your activity level, you can determine the appropriate calorie intake for weight maintenance, loss, or gain.</p>
+        <h3>Supporting Muscle Growth and Recovery</h3>
+        <p>Proper macronutrient distribution is crucial for supporting muscle growth and enhancing recovery after intense workouts. Learning about macro splits helps you understand the role of protein, carbohydrates, and fats in muscle repair, glycogen replenishment, and overall performance.</p>
+        <h3>Maximizing Performance</h3>
+        <p>By optimizing your nutrition based on BMR and macro split recommendations, you can fuel your body effectively for workouts and physical activities. This can lead to improved strength, endurance, and overall athletic performance.</p>
+        <h3>Achieving Body Composition Goals</h3>
+        <p>Whether your goal is to build muscle, lose fat, or achieve a balanced physique, understanding BMR and macro split principles empowers you to create a customized nutrition plan aligned with your objectives. This allows for more efficient progress towards your desired body composition.</p>
+        <p>Ultimately, learning about BMR and macro split not only enhances your understanding of nutrition but also empowers you to make informed choices that support your fitness journey, leading to improved performance, body composition, and overall well-being.</p>
+
+    </div>
+  )
+}
+
+export default Graph
