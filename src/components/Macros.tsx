a4fffc2d6f699ce56c6379609fd48236f7ef34a1
dagkand <dagkandersen@gmail.com>
2024-04-26 09:01:42 +0200

    temp info

---
+++ src/components/Macros.tsx
@@ -0,0 +1,29 @@
+import '../App.css'
+
+function About() {
+
+  return (
+    <div className='macrosinfo'>
+        <h2>Importance of BMR and Macro Split</h2>
+        <p>Understanding your Basal Metabolic Rate (BMR) and macronutrient split is crucial for optimizing your nutrition and achieving your health and fitness goals.</p>
+        <h3>BMR</h3>
+        <p>Your BMR represents the number of calories your body needs to maintain basic physiological functions while at rest. It's essential to know your BMR because:</p>
+        <ul>
+        <li>It forms the foundation for calculating your daily calorie needs.</li>
+        <li>Knowing your BMR helps you create a personalized nutrition plan tailored to your specific energy requirements.</li>
+        <li>It enables you to adjust your calorie intake for weight loss, maintenance, or muscle gain goals.</li>
+        </ul>
+        <h3>Macro Split</h3>
+        <p>A macro split refers to the distribution of macronutrients (protein, carbohydrates, and fats) in your diet. Understanding your macro split is important because:</p>
+        <ul>
+        <li>Each macronutrient plays a distinct role in your body's functions, energy production, and overall health.</li>
+        <li>Optimizing your macro split can help you achieve better performance in workouts, improve recovery, and support muscle growth.</li>
+        <li>It allows for more precise control over your nutrient intake, which is essential for reaching specific fitness goals.</li>
+        </ul>
+        <p>By combining knowledge of your BMR with an appropriate macro split, you can develop a nutrition strategy that supports your individual needs, promotes overall well-being, and enhances your fitness journey.</p>
+
+    </div>
+  )
+}
+
+export default About
