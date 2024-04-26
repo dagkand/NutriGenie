7bf100dd53df1372cd7e740688996e95d4dd82ed
dagkand <dagkandersen@gmail.com>
2024-04-26 08:42:53 +0200

    add navbar pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,9 +7,9 @@ function App() {
     <div className='navigation'>
         <ul className="pages">
             <li><Link to='/'>Home</Link></li>
-            <li><Link to='/cats'>Cats</Link></li>
-            <li><Link to='/dogs'>Dogs</Link></li>
-            <li><Link to='/birds'>Birds</Link></li>
+            <li><Link to='/Positive'>Why?</Link></li> {/* Why should you have knowledge of BMR and macro split? */}
+            <li><Link to='/Studies'>How?</Link></li> {/* How calculations and macro split is suggested */}
+            <li><Link to='/About'>About</Link></li> {/* Temporary for additional info */}
          </ul>
     </div>
   )
