4157923b47be8e2cf7ec239b6e823bd7e57b9985
dagkand <dagkandersen@gmail.com>
2024-04-26 08:58:25 +0200

    reformat structure

---
+++ src/pages/Home.tsx
@@ -0,0 +1,19 @@
+import Navbar from '../components/Navbar'
+import Title from '../components/Title'
+import Calculator from '../components/Calculator'
+import Results from '../components/Results'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <Navbar />
+      <Title />
+      <Calculator />
+      <Results />
+    </>
+  )
+}
+
+export default App
