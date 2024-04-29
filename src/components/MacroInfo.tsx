e06934b258f185baa9c2da2b024db2a56cfe7c07
dagkand <dagkandersen@gmail.com>
2024-04-29 13:03:14 +0200

    more text

--- src/components/MacroInfo.tsx
+++ src/components/MacroInfo.tsx
@@ -4,30 +4,29 @@ function Results() {
 
   return (
     <div className='calories'>
-      
-<h2>Understanding Macros: The Essential Components of Nutrition</h2>
-<div>
-    <h3>Introduction to Macros</h3>
-    <p>Macros, short for macronutrients, are the fundamental building blocks of nutrition. They are comprised of three main components: carbohydrates, proteins, and fats. Each macro plays a crucial role in providing energy and supporting various bodily functions.</p>
-    <p>Calories serve as the measurement unit for energy obtained from consuming macros. Understanding the calorie content of each macro type is essential for maintaining a balanced diet.</p>
-</div>
-<div>
-    <h3>Carbohydrates</h3>
-    <p>Carbohydrates are the primary source of energy for the body. Each gram of carbohydrates provides approximately 4 calories. They come in two main forms: simple carbohydrates (sugars) and complex carbohydrates (starches and fibers). While simple carbs offer quick energy bursts, complex carbs provide sustained energy levels and support digestive health.</p>
-</div>
-<div>
-    <h3>Proteins</h3>
-    <p>Proteins are essential for building and repairing tissues, as well as supporting immune function and hormone regulation. Like carbohydrates, proteins also provide approximately 4 calories per gram. They are composed of amino acids, which are the building blocks of life. Consuming an adequate amount of protein is crucial for maintaining muscle mass and promoting overall health.</p>
-</div>
-<div>
-    <h3>Fats</h3>
-    <p>Despite their often-misunderstood reputation, fats are vital for various bodily functions, including hormone production, nutrient absorption, and insulation. They are the most energy-dense macro, providing approximately 9 calories per gram. Fats are categorized into saturated, unsaturated, and trans fats, with each type having distinct effects on health. It's important to prioritize healthy fats, such as those found in avocados, nuts, and olive oil, while minimizing intake of trans fats found in processed foods.</p>
-</div>
-<div>
-    <h3>Optimizing Macro Balance</h3>
-    <p>Just as a car requires the right blend of gasoline components for optimal performance, our bodies thrive when we consume the right balance of macros for our nutritional needs. This entails understanding our individual energy requirements and adjusting our macro intake accordingly. By prioritizing nutrient-dense foods and maintaining a balanced diet, we can fuel our bodies effectively and support overall health and well-being.</p>
-</div>
-  </div>
+      <h2>Understanding Macros: The Essential Components of Nutrition</h2>
+      <div>
+          <h3>Introduction to Macros</h3>
+          <p>Macros, short for macronutrients, are the fundamental building blocks of nutrition. They are comprised of three main components: carbohydrates, proteins, and fats. Each macro plays a crucial role in providing energy and supporting various bodily functions.</p>
+          <p>Calories serve as the measurement unit for energy obtained from consuming macros. Understanding the calorie content of each macro type is essential for maintaining a balanced diet.</p>
+      </div>
+      <div>
+          <h3>Carbohydrates</h3>
+          <p>Carbohydrates are the primary source of energy for the body. Each gram of carbohydrates provides approximately 4 calories. They come in two main forms: simple carbohydrates (sugars) and complex carbohydrates (starches and fibers). While simple carbs offer quick energy bursts, complex carbs provide sustained energy levels and support digestive health.</p>
+      </div>
+      <div>
+          <h3>Proteins</h3>
+          <p>Proteins are essential for building and repairing tissues, as well as supporting immune function and hormone regulation. Like carbohydrates, proteins also provide approximately 4 calories per gram. They are composed of amino acids, which are the building blocks of life. Consuming an adequate amount of protein is crucial for maintaining muscle mass and promoting overall health.</p>
+      </div>
+      <div>
+          <h3>Fats</h3>
+          <p>Despite their often-misunderstood reputation, fats are vital for various bodily functions, including hormone production, nutrient absorption, and insulation. They are the most energy-dense macro, providing approximately 9 calories per gram. Fats are categorized into saturated, unsaturated, and trans fats, with each type having distinct effects on health. It's important to prioritize healthy fats, such as those found in avocados, nuts, and olive oil, while minimizing intake of trans fats found in processed foods.</p>
+      </div>
+      <div>
+          <h3>Optimizing Macro Balance</h3>
+          <p>Just as a car requires the right blend of gasoline components for optimal performance, our bodies thrive when we consume the right balance of macros for our nutritional needs. This entails understanding our individual energy requirements and adjusting our macro intake accordingly. By prioritizing nutrient-dense foods and maintaining a balanced diet, we can fuel our bodies effectively and support overall health and well-being.</p>
+      </div>
+    </div>
   )
 }
 
--- src/components/Motivation.tsx
+++ src/components/Motivation.tsx
@@ -4,19 +4,15 @@ function Motivation() {
 
   return (
     <div className='motivation'>
-        <h2>Motivation for Learning BMR and Macro Split for Fitness</h2>
-        <p>Understanding concepts such as Basal Metabolic Rate (BMR) and macronutrient splits is essential for individuals pursuing fitness and bodybuilding goals. Here's why:</p>
-        <h3>Optimizing Nutrition</h3>
-        <p>Knowledge of your BMR enables you to tailor your calorie intake to match your body's energy needs. By calculating your BMR and considering your activity level, you can determine the appropriate calorie intake for weight maintenance, loss, or gain.</p>
-        <h3>Supporting Muscle Growth and Recovery</h3>
-        <p>Proper macronutrient distribution is crucial for supporting muscle growth and enhancing recovery after intense workouts. Learning about macro splits helps you understand the role of protein, carbohydrates, and fats in muscle repair, glycogen replenishment, and overall performance.</p>
-        <h3>Maximizing Performance</h3>
-        <p>By optimizing your nutrition based on BMR and macro split recommendations, you can fuel your body effectively for workouts and physical activities. This can lead to improved strength, endurance, and overall athletic performance.</p>
-        <h3>Achieving Body Composition Goals</h3>
-        <p>Whether your goal is to build muscle, lose fat, or achieve a balanced physique, understanding BMR and macro split principles empowers you to create a customized nutrition plan aligned with your objectives. This allows for more efficient progress towards your desired body composition.</p>
-        <p>Ultimately, learning about BMR and macro split not only enhances your understanding of nutrition but also empowers you to make informed choices that support your fitness journey, leading to improved performance, body composition, and overall well-being.</p>
-
+      <h2>About Me: Bridging Information Technology and Fitness</h2>
+      <p>As an Informatikk student at NTNU with a passion for both technology and fitness, I've embarked on a journey to merge these two worlds into one cohesive project. This endeavor stems from my belief that the intersection of technology and fitness holds immense potential for personal growth and optimization.</p>
+      <h3>Empowering Personal Transformation</h3>
+      <p>My fascination with fitness isn't just about physical appearance; it's about embracing a lifestyle that fosters holistic well-being and self-improvement. Through my studies in information technology and my dedication to fitness, I've come to realize the transformative power of knowledge and data-driven decision-making.</p>
+      <p>Understanding concepts such as Basal Metabolic Rate (BMR) and macronutrient splits is essential for individuals pursuing fitness and bodybuilding goals. By calculating your BMR and considering your activity level, you can determine the appropriate calorie intake for weight maintenance, loss, or gain. Proper macronutrient distribution is crucial for supporting muscle growth and enhancing recovery after intense workouts.</p>
+      <p>This project is not only a reflection of my own quest for self-improvement but also an endeavor to share valuable insights with others who are on a similar path. By providing simple tools for calculating calories and macros, I aim to empower individuals to make informed decisions about their nutrition and fitness regimens.</p>
+      <p>Ultimately, this project is a testament to the synergy between technology and fitness, and the profound impact they can have on our lives when combined with intention and purpose.</p>
     </div>
+
   )
 }
 
